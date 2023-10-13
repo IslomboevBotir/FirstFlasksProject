22f6c817990fc86f557bca9319998ec01c4ed195
islomboev <islomboev_botir@mail.ru>
2023-10-13 14:51:10 +0500

    added treatment csv

--- flaskr/csv/treatment.py
+++ flaskr/csv/treatment.py
@@ -1,8 +1,32 @@
+
+from datetime import datetime
 import pandas as pd
-from sqlalchemy import create_engine
 
 
-def open_csv(csv_file: str):
-    dataframe = pd.read_csv(csv_file)
-    dataframe_to_dict = dataframe.to_dict(orient="records")
-
+def parse_in_data_base(engine, read_csv, query):
+    csv_in_dict = read_csv.to_dict(orient="records")
+    count = 0
+    with engine.connect() as connection:
+        for data in csv_in_dict:
+            data['DATE'] = datetime.strptime(data['DATE'], '%d.%m.%Y').strftime('%Y-%m-%d')
+            if pd.isna(data['IS_MODE']):
+                data['IS_MODE'] = None
+            if pd.isna(data['IS_DEL']):
+                data['IS_DEL'] = None
+            data = {
+                'cid': data['CID'],
+                'unit': data['UNIT'],
+                'w_id': data['W_ID'],
+                'utype': data['UTYPE'],
+                'beds': data['BEDS'],
+                'area': data['AREA'],
+                'price': data['PRICE'],
+                'date': data['DATE'],
+                'is_mode': data['IS_MODE'],
+                'is_del': data['IS_DEL'],
+            }
+            connection.execute(query, data)
+            count += 1
+            if count % 10 == 0:
+                connection.commit()
+        connection.commit()
--- flaskr/run.py
+++ flaskr/run.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Text, Date, Boolean, Float, text, func
+from flaskr.csv.treatment import parse_in_data_base
 from serializer import ProjectSchema
 import pandas as pd
 from datetime import datetime
@@ -8,35 +9,6 @@ from models import DataBaseProject
 from database import db, app
 
 project_schema = ProjectSchema()
-
-
-def parse_in_data_base(engine, read_csv, query):
-    csv_in_dict = read_csv.to_dict(orient="records")
-    count = 0
-    with engine.connect() as connection:
-        for data in csv_in_dict:
-            data['DATE'] = datetime.strptime(data['DATE'], '%d.%m.%Y').strftime('%Y-%m-%d')
-            if pd.isna(data['IS_MODE']):
-                data['IS_MODE'] = None
-            if pd.isna(data['IS_DEL']):
-                data['IS_DEL'] = None
-            data = {
-                'cid': data['CID'],
-                'unit': data['UNIT'],
-                'w_id': data['W_ID'],
-                'utype': data['UTYPE'],
-                'beds': data['BEDS'],
-                'area': data['AREA'],
-                'price': data['PRICE'],
-                'date': data['DATE'],
-                'is_mode': data['IS_MODE'],
-                'is_del': data['IS_DEL'],
-            }
-            connection.execute(query, data)
-            count += 1
-            if count % 10 == 0:
-                connection.commit()
-        connection.commit()
 
 
 @app.route('/')
